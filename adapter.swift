ee8cc084adea33b60b9a6c8ff2501bd276fe16fc
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-04-20 18:16:45 +0300

    Create adapter.swift

---
+++ adapter.swift
@@ -0,0 +1,37 @@
+protocol NewDeathStarSuperLaserAiming {
+    var angleV: Double { get }
+    var angleH: Double { get }
+}
+
+struct OldDeathStarSuperlaserTarget {
+    let angleHorizontal: Float
+    let angleVertical: Float
+
+    init(angleHorizontal: Float, angleVertical: Float) {
+        self.angleHorizontal = angleHorizontal
+        self.angleVertical = angleVertical
+    }
+}
+
+struct NewDeathStarSuperlaserTarget: NewDeathStarSuperLaserAiming {
+
+    private let target: OldDeathStarSuperlaserTarget
+
+    var angleV: Double {
+        return Double(target.angleVertical)
+    }
+
+    var angleH: Double {
+        return Double(target.angleHorizontal)
+    }
+
+    init(_ target: OldDeathStarSuperlaserTarget) {
+        self.target = target
+    }
+}
+
+let target = OldDeathStarSuperlaserTarget(angleHorizontal: 14.0, angleVertical: 12.0)
+let newFormat = NewDeathStarSuperlaserTarget(target)
+
+newFormat.angleH
+newFormat.angleV
