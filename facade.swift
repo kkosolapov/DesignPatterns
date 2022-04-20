e6df811ac46afe72d402ebcd32cb44a5bc568bff
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-04-20 18:19:48 +0300

    Create facade.swift

---
+++ facade.swift
@@ -0,0 +1,24 @@
+final class Defaults {
+
+    private let defaults: UserDefaults
+
+    init(defaults: UserDefaults = .standard) {
+        self.defaults = defaults
+    }
+
+    subscript(key: String) -> String? {
+        get {
+            return defaults.string(forKey: key)
+        }
+
+        set {
+            defaults.set(newValue, forKey: key)
+        }
+    }
+}
+
+let storage = Defaults()
+
+storage["Bishop"] = "Disconnect me. I’d rather be nothing"
+
+storage["Bishop"]
