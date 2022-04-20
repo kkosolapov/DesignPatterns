4be09ccdfdcf7bccb30e9283f464826b94ae3c41
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-04-20 18:17:28 +0300

    Create bridge.swift

---
+++ bridge.swift
@@ -0,0 +1,38 @@
+protocol Switch {
+    var appliance: Appliance { get set }
+    func turnOn()
+}
+
+protocol Appliance {
+    func run()
+}
+
+final class RemoteControl: Switch {
+    var appliance: Appliance
+
+    func turnOn() {
+        self.appliance.run()
+    }
+    
+    init(appliance: Appliance) {
+        self.appliance = appliance
+    }
+}
+
+final class TV: Appliance {
+    func run() {
+        print("tv turned on");
+    }
+}
+
+final class VacuumCleaner: Appliance {
+    func run() {
+        print("vacuum cleaner turned on")
+    }
+}
+
+let tvRemoteControl = RemoteControl(appliance: TV())
+tvRemoteControl.turnOn()
+
+let fancyVacuumCleanerRemoteControl = RemoteControl(appliance: VacuumCleaner())
+fancyVacuumCleanerRemoteControl.turnOn()
