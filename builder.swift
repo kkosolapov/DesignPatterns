24acce36546acf0fbc39a67b5e4163d397858430
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-04-20 18:14:13 +0300

    Create builder.swift

---
+++ builder.swift
@@ -0,0 +1,42 @@
+final class DeathStarBuilder {
+
+    var x: Double?
+    var y: Double?
+    var z: Double?
+
+    typealias BuilderClosure = (DeathStarBuilder) -> ()
+
+    init(buildClosure: BuilderClosure) {
+        buildClosure(self)
+    }
+}
+
+struct DeathStar : CustomStringConvertible {
+
+    let x: Double
+    let y: Double
+    let z: Double
+
+    init?(builder: DeathStarBuilder) {
+
+        if let x = builder.x, let y = builder.y, let z = builder.z {
+            self.x = x
+            self.y = y
+            self.z = z
+        } else {
+            return nil
+        }
+    }
+
+    var description:String {
+        return "Death Star at (x:\(x) y:\(y) z:\(z))"
+    }
+}
+
+let empire = DeathStarBuilder { builder in
+    builder.x = 0.1
+    builder.y = 0.2
+    builder.z = 0.3
+}
+
+let deathStar = DeathStar(builder:empire)
