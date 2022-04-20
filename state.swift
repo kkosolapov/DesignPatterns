e11fee620c018bf48e0ab6dfac25072d9c55e85f
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-04-20 18:11:31 +0300

    Update state.swift

--- state.swift
+++ state.swift
@@ -42,6 +42,6 @@ class AuthorizedState: State {
 let userContext = Context()
 (userContext.isAuthorized, userContext.userId)
 userContext.changeStateToAuthorized(userId: "admin")
-(userContext.isAuthorized, userContext.userId) // now logged in as "admin"
+(userContext.isAuthorized, userContext.userId)
 userContext.changeStateToUnauthorized()
 (userContext.isAuthorized, userContext.userId)
