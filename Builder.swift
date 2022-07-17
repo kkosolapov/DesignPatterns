1c225b35c8a2930b8cab6d851a89f38773cab467
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-07-17 10:04:31 +0300

    Update Builder.swift

--- Builder.swift
+++ Builder.swift
@@ -1,42 +1,85 @@
-final class DeathStarBuilder {
+import Foundation
 
-    var x: Double?
-    var y: Double?
-    var z: Double?
+struct Laptop {
+    let name: String
+    let processor: Processor
+    
+    let rating: Rating
+}
 
-    typealias BuilderClosure = (DeathStarBuilder) -> ()
+extension Laptop {
+	var description: String {
+		return "\(name) is a \(rating.displayName) Laptop Packed with a \(processor) Processor"
+	}
+}
 
-    init(buildClosure: BuilderClosure) {
-        buildClosure(self)
+enum Processor {
+    case Intel
+    case M1
+    case AMD
+}
+
+enum Rating {
+	case mustBuy
+	case mediocre
+   	case awful
+}
+
+extension Rating {
+	var displayName: String {
+		switch self {
+		case .mustBuy: return "Must Buy"
+		case .mediocre: return "Mediocre"
+		case .awful: return "Awful"
+		}
+	}
+}
+
+class LaptopBuilder {
+    private var name: String? = "Laptop"
+    private var processor: Processor? = .Intel
+    private var rating: Rating? = .mediocre
+    
+    @discardableResult
+    func setName(to newName: String) -> Self {
+    	self.name = newName
+    	return self
+    }
+    
+    @discardableResult
+    func setProcessor(to newProcessor: Processor) -> Self {
+    	self.processor = newProcessor
+    	return self
+    }
+    
+    @discardableResult
+    func setRating(to newRating: Rating) -> Self {
+    	self.rating = newRating
+    	return self
+    }
+	
+	func build() -> Laptop {
+		return Laptop(
+			name: name ?? "Laptop",
+			processor: processor ?? .Intel,
+			rating: rating ?? .mediocre
+		)
+	}
+}
+
+
+// @main
+struct Main {
+
+    static func main() {
+        let laptop = LaptopBuilder()
+        	.setName(to: "Macbook Air")
+        	.setProcessor(to: .M1)
+        	.setRating(to: .mustBuy)
+        	.build()
+        
+        print(laptop.description)
     }
 }
 
-struct DeathStar : CustomStringConvertible {
-
-    let x: Double
-    let y: Double
-    let z: Double
-
-    init?(builder: DeathStarBuilder) {
-
-        if let x = builder.x, let y = builder.y, let z = builder.z {
-            self.x = x
-            self.y = y
-            self.z = z
-        } else {
-            return nil
-        }
-    }
-
-    var description:String {
-        return "Death Star at (x:\(x) y:\(y) z:\(z))"
-    }
-}
-
-let empire = DeathStarBuilder { builder in
-    builder.x = 0.1
-    builder.y = 0.2
-    builder.z = 0.3
-}
-
-let deathStar = DeathStar(builder:empire)
+Main.main()
